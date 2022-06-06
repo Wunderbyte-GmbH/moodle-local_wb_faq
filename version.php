1323fdad7d4d7b813ea2ea98234772d86de6d49c
georgmaisser <georg.maisser@wunderbyte.at>
2022-06-06 22:21:11 +0200

    Version  0.1.2 (2022060600)

--- version.php
+++ version.php
@@ -26,8 +26,8 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_wb_faq';
-$plugin->release = '0.1.1';
-$plugin->version = 2022060200;
+$plugin->release = '0.1.2';
+$plugin->version = 2022060600;
 $plugin->requires = 2020061500;
 $plugin->supported = [39, 311];
 $plugin->maturity = MATURITY_ALPHA;
