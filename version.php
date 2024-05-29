1c0001f4e09a8f1ba4ce258625f4d1ac9016b2a9
Georg Maißer <georg.maisser@wunderbyte.at>
2024-05-29 13:08:28 +0200

    Version (2024052900)

--- version.php
+++ version.php
@@ -27,7 +27,7 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_wb_faq';
 $plugin->release = '0.2.7';
-$plugin->version = 2024051404;
+$plugin->version = 2024052900;
 $plugin->requires = 2020061500;
 $plugin->supported = [39, 43];
 $plugin->maturity = MATURITY_ALPHA;
